42708c56055302789d69e5ee3e1a9beebca7f772
Egor Ponomarev <egor@ponomarev.pp.ru>
2017-09-27 19:23:47 +0300

    home

--- data-base.js
+++ data-base.js
@@ -1,6 +1,12 @@
 var mongodb = require('mongodb');
+var express = require('express');
 var MongoClient = mongodb.MongoClient;
 var url = 'mongodb://localhost:80800/my_database';
+
+const app = express();
+app.listen(1337);
+
+const restV1 = express.Router();
 
 MongoClient.connect(url, (err, db) => {
   if (err) {
@@ -8,86 +14,77 @@ MongoClient.connect(url, (err, db) => {
   } else {
     console.log(`Соединение установлено с ${url}`);
 
-    contact_1 = { '_id': 1, 'fname': 'Aziz', 'sname': 'Kerimov', 'phone': '+7 929 594 40 67' };
-    contact_2 = { '_id': 2, 'fname': 'Anton', 'sname': 'Dimitriev', 'phone': '+7 917 591 13 90' };
-    contact_3 = { '_id': 3, 'fname': 'Alexey', 'sname': 'Ponomarev', 'phone': '+7 917 231 00 80' };
-    contact_4 = { '_id': 4, 'fname': 'Valentina', 'sname': 'Ponomareva', 'phone': '+7 927 498 50 01' };
-    contact_5 = { '_id': 5, 'fname': 'Max', 'sname': 'Belyaev', 'phone': '+7 967 085 52 43' };
-    contact_6 = { '_id': 6, 'fname': 'Regina', 'phone': '+7 962 574 23 45' };
+    contact_1 = {'fname': 'Aziz', 'sname': 'Kerimov', 'phone': '+7 929 594 40 67' };
+    contact_2 = {'fname': 'Anton', 'sname': 'Dimitriev', 'phone': '+7 917 591 13 90' };
+    contact_3 = {'fname': 'Alexey', 'sname': 'Ponomarev', 'phone': '+7 917 231 00 80' };
+    contact_4 = {'fname': 'Valentina', 'sname': 'Ponomareva', 'phone': '+7 927 498 50 01' };
+    contact_5 = {'fname': 'Max', 'sname': 'Belyaev', 'phone': '+7 967 085 52 43' };
+    contact_6 = {'fname': 'Regina', 'phone': '+7 962 574 23 45' };
 
-    function makeCollection(name, mass) {
-      return new Promise((resolve, reject) => {
-        var collection = db.collection(name);
-        collection.insert(mass);
+    const mass = [
+      contact_1, contact_2, contact_3, contact_4, contact_5, contact_6
+    ]
 
-        resolve(collection);
-      })
-    }
+    restV1.get('/:name', (req, res) => {
+      var collection = db.collection(req.params.name);
+      collection.insert(mass);
 
-    function newContact(collection, contact) {
-      return new Promise((resolve, reject) => {
-        collection.insert(contact);
-        console.log(`${contact.fname} успешно добавлен в телефонную книгу`)
+      res.json(collection);
+      res.sendStatus(200);
+    });
 
-        resolve(collection);
-      })
-    }
+    restV1.get('/', (req, res) => {
+      res.json(collection);
+      res.sendStatus(200);
+    });
 
-    function updatePhBook(collection, who, what) {
-      return new Promise((resolve, reject) => {
-        collection.update(who, { $set: what });
-        console.log('Обновление контакта успешно завершено')
-
-        resolve(collection);
-      })
-    }
-
-    function removeContact(collection, who) {
-      return new Promise((resolve, reject) => {
-        var contact = collection.find(who);
-        collection.remove(who);
-        console.log(`Контакт ${contact} успешно удален`)
-
-
-        resolve(collection);
-      })
-    }
-
-    function consolePhBook(collection) {
-      return new Promise((resolve, reject) => {
-        collection.forEach(el => {
-          console.log(`Имя: ${el.fname} телефон: ${el.phone}`)
-        })
-
-        resolve(collection)
-      })
-    }
-
-    function findContact(collection, who) {
-      return new Promise((resolve, reject) => {
-        var contact = collection.find(who) ? collection.find(who) : undefined;
-        if (contact !== undefined) {
-          console.log('Контакт успешно найден');
-        } else {
-          console.log('Контакт не найден');
+    restV1.post('/new/:fname/sname/:sname/phone/:phone', (req, res) => {
+      if(req.params.fname && req.params.sname && req.params.phone) {
+        const new_contact = {
+          'fname': req.params.fname, 'sname': req.params.sname, 'phone': req.params.phone
         }
-        resolve(contact);
-      })
-    }
+        collection.insert(new_contact);
 
-    makeCollection('Phonebook', [contact_1, contact_2, contact_3, contact_4,
-        contact_5, contact_6
-      ])
-      .then(consolePhBook)
-      .then(collection => newContact(collection, { '_id': 7, 'fname': 'Ildar', 'sname': 'Gateev', 'phone': '+7 963 125 75 58' }))
-      .then(collection => updatePhBook(collection, { 'fname': 'Alexey' }, { 'phone': '+7 960 081 07 93' }))
-      .then(collection => removeContact(collection, { 'fname': 'Regina' }))
-      .then(consolePhBook)
-      .then(findContact({ 'sname': 'Ponomarev' }))
-      .then(consolePhBook);
+        res.json(collection);
+        res.sendStatus(200);
+      } else {
+        res.sendStatus(404);
+      }
+    });
 
+    restV1.put('/update/:who/att/:att/vul/:vul', (req, res) => {
+      if(req.params.who && req.params.att && req.params.vul) {
+        collection.update({ 'fname' : req.params.who }, { '$set': { req.params.att : req.params.vul } });
 
+        res.json(collection);
+        res.sendStatus(200);
+      } else {
+        res.sendStatus(404);
+      }
 
+    });
+    
+    restV1.delete('/remove/:name', (req, res) => {
+      if (req.params.name) {
+        collection.remove({ 'fname' : req.params.name });
+
+        res.json(collection);
+        res.sendStatus(200);
+      } else {
+        res.sendStatus(404);
+      }
+    });
+
+    restV1.get('/find/:name', (req, res) => {
+      if(req.params.name && collection.find({ 'fname' : req.params.name })) {
+        res.json(collection.find({ 'fname' : req.params.name }));
+        res.sendStatus(200);
+      } else {
+        res.sendStatus(404);
+      }
+    })
   }
   db.close();
 })
+
+app.use('/api/v1', restV1);
